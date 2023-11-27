b14e046d4dbead15f8c6ef69a2da722e594e8d35
Dormant512 <mikegrig@inbox.ru>
2023-11-27 16:01:57 +0300

    path upd

--- gossip.go
+++ gossip.go
@@ -4,17 +4,16 @@ import (
 	"fmt"
 	"gopkg.in/yaml.v3"
 	"os"
-	"time"
 )
 
 type Config struct {
-	Port    int           `yaml:"port"`
-	Host    string        `yaml:"host"`
-	Timeout time.Duration `yaml:"timeout"`
+	Port      int    `yaml:"port"`
+	Host      string `yaml:"host"`
+	TimeoutMS int    `yaml:"timeout"`
 }
 
 func YamlToConfig(path string) (Config, error) {
-	yamlFile, err := os.ReadFile("items.yaml")
+	yamlFile, err := os.ReadFile(path)
 	if err != nil {
 		return Config{}, err
 	}
